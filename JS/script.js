6616c21c774a0750e57af8b93a71042e77bb0e13
Thalissom Vinicius <102325856+thalissomvinicius@users.noreply.github.com>
2024-04-04 12:32:21 -0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,59 @@
+const imgs = document.querySelectorAll('.header-slide ul img');
+const prev_btn = document.querySelector('.control_prev');
+const next_btn = document.querySelector('.control_next');
+
+let n = 0;
+
+function changeSlide() {
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.display = 'none';
+    }
+    imgs[n].style.display = 'block';
+}
+
+changeSlide();
+
+prev_btn.addEventListener('click', (e) => {
+    if (n > 0) {
+        n--;
+    } else {
+        n = imgs.length - 1;
+    }
+    changeSlide();
+})
+next_btn.addEventListener('click', (e) => {
+    if (n < imgs.length - 1) {
+        n++;
+    } else {
+        n = 0;
+    }
+    changeSlide();
+})
+
+const scrollContainer = document.querySelectorAll('.produtos');
+
+for (const item of scrollContainer) {
+    item.addEventListener('wheel', (evt) => {
+        evt.preventDefault();
+        item.scrollLeft += evt.deltaY;
+    });
+}
+
+const backtop = document.querySelector(".backtop");
+backtop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+})
+
+const sidebar = document.querySelector(".sidebar");
+const cross = document.querySelector(".fa-xmark");
+const black = document.querySelector(".black");
+const sidebtn = document.querySelector(".second-1");
+
+sidebtn.addEventListener("click", () => {
+    sidebar.classList.add("active");
+    cross.classList.add("active");
+    black.classList.add("active");
+})
